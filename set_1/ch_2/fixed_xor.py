299470f66e12dfedd3be9093a2944ed76059897e
Peter Wagenaar <sakartu@gmail.com>
2015-03-09 17:48:52 +0100

    Initial commit for fixed_xor

---
+++ set_1/ch_2/fixed_xor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+"""
+Usage:
+fixed_xor.py S1 S2
+
+Options:
+"""
+import binascii
+from docopt import docopt
+
+__author__ = 'peter'
+
+
+def main():
+    args = docopt(__doc__)
+    bs1 = bytearray.fromhex(args['S1'])
+    bs2 = bytearray.fromhex(args['S2'])
+    result = bytearray()
+    for b1, b2 in zip(bs1, bs2):
+        result.append(b1 ^ b2)
+    print(binascii.hexlify(result).decode())
+
+
+if __name__ == '__main__':
+    main()
