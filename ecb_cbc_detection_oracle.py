7ba4065bb885888ace9d1d455cce4e2ec1b6ca65
Peter Wagenaar <sakartu@gmail.com>
2015-03-17 16:47:37 +0100

    Add stub for ecb/cbc detection oracle

---
+++ ecb_cbc_detection_oracle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+"""
+Usage:
+ecb_cbc_detection_oracle.py
+
+Options:
+"""
+
+from docopt import docopt
+
+__author__ = 'peter'
+
+
+def main():
+    args = docopt(__doc__)
+
+
+if __name__ == '__main__':
+    main()
