afd08b0abbf7bac3e97215d2040b7f5315afd5c9
Peter Wagenaar <sakartu@gmail.com>
2015-03-19 15:50:16 +0100

    Fix ecb/cbc detection

--- set_2/ch_11/detect_ecb_cbc.py
+++ set_2/ch_11/detect_ecb_cbc.py
@@ -21,11 +21,12 @@ modes = {
 
 def main():
     print('detect', 'real')
-    for i in range(10):
+    for i in range(1000):
         plain = b'A' * 1000
         real_mode, ct = util.encryption_oracle(plain)
         detect_mode = util.detect_ecb_or_cbc(ct)
         print(modes[detect_mode], modes[real_mode], 'V' if real_mode == detect_mode else 'X')
+        assert detect_mode == real_mode
 
 
 if __name__ == '__main__':
