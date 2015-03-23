11b2037d80139bfd4ce089129b6e5f486cc8d839
Peter Wagenaar <sakartu@gmail.com>
2015-03-23 16:36:47 +0100

    Initial byte-by-byte hard version

---
+++ set_2/ch_14/byte_by_byte_ecb_hard.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+"""
+Usage:
+byte_by_byte_ecb_hard.py
+
+Options:
+"""
+
+from docopt import docopt
+
+__author__ = 'peter'
+
+
+def main():
+    args = docopt(__doc__)
+
+
+if __name__ == '__main__':
+    main()
