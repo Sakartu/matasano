88c76285666b1e6beb21eebe260e85d57aa984bd
Peter Wagenaar <sakartu@gmail.com>
2015-03-24 12:28:32 +0100

    Add some tests for pkcs7 depad

--- set_2/ch_15/test_pkcs7_depad.py
+++ set_2/ch_15/test_pkcs7_depad.py
@@ -3,17 +3,30 @@
 """
 Usage:
 test_pkcs7_depad.py
-
-Options:
 """
 
 from docopt import docopt
+import util
 
 __author__ = 'peter'
 
 
 def main():
-    args = docopt(__doc__)
+    cases = [
+        (b"ICE ICE BABY\x04\x04\x04\x04", True),
+        (b"ICE ICE BABY\x05\x05\x05\x05", False),
+        (b"ICE ICE BABY\x01\x02\x03\x04", False),
+        (b"ICE ICE BABY", False),
+    ]
+    for s, r in cases:
+        if r:
+            assert util.pkcs7_depad(s) == b"ICE ICE BABY"
+        else:
+            try:
+                util.pkcs7_depad(s)
+                raise AssertionError()
+            except ValueError:
+                pass
 
 
 if __name__ == '__main__':
