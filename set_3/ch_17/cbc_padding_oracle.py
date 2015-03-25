40682bc42ee345fc2408fe9b7fe9ca544bed5f3b
Peter Wagenaar <sakartu@gmail.com>
2015-03-25 09:44:03 +0100

    Create ch 17 specific code

---
+++ set_3/ch_17/cbc_padding_oracle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+"""
+Usage:
+cbc_padding_oracle
+
+Options:
+"""
+import base64
+import random
+
+from docopt import docopt
+import util
+
+__author__ = 'peter'
+
+
+def main():
+    args = docopt(__doc__)
+
+
+def cbc_enc_ch_17():
+    pt = random.choice([base64.b64decode(s) for s in (
+        'MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=',
+        'MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=',
+        'MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==',
+        'MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==',
+        'MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl',
+        'MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==',
+        'MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==',
+        'MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=',
+        'MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=',
+        'MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93')
+                        ])
+    iv = util.get_random_bytes(16)
+    return iv, util.aes_cbc_encrypt(pt, util.GLOBAL_KEY, iv=iv)
+
+
+def cbc_dec_ch_17(ct, iv):
+    try:
+        util.aes_cbc_decrypt(ct, util.GLOBAL_KEY, iv)
+        return True
+    except ValueError:
+        return False
+
+
+
+
+if __name__ == '__main__':
+    main()
--- util.py
+++ util.py
@@ -263,30 +263,3 @@ def find_repeating_block(ct, bs, minlen=2):
             blocks.append((begin, end))
         begin = end + 1
     return blocks
-
-
-def cbc_enc_ch_17():
-    pt = random.choice([base64.b64decode(s) for s in (
-        'MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=',
-        'MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=',
-        'MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==',
-        'MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==',
-        'MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl',
-        'MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==',
-        'MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==',
-        'MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=',
-        'MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=',
-        'MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93')
-    ])
-    iv = get_random_bytes(16)
-    return iv, aes_cbc_encrypt(pt, GLOBAL_KEY, iv=iv)
-
-
-def cbc_dec_ch_17(ct, iv):
-    try:
-        aes_cbc_decrypt(ct, GLOBAL_KEY, iv)
-        return True
-    except ValueError:
-        return False
-
-
