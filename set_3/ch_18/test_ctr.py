1763297dcf349081ff07f21325ba17bec1d32aa9
Peter Wagenaar <sakartu@gmail.com>
2015-03-26 17:26:56 +0100

    Add some more CTR mode test code

--- set_3/ch_18/test_ctr.py
+++ set_3/ch_18/test_ctr.py
@@ -13,7 +13,10 @@ __author__ = 'peter'
 
 def main():
     test = base64.b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==')
-    print(util.aes_ctr_decrypt(test, b"YELLOW SUBMARINE"))
+    assert util.aes_ctr_decrypt(test, b"YELLOW SUBMARINE") == b"Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby "
+    k = util.get_random_bytes(16)
+    m = b'This is an interesting message'
+    assert util.aes_ctr_decrypt(util.aes_ctr_encrypt(m, k), k) == m
 
 
 if __name__ == '__main__':
