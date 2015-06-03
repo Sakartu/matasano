9a6ef33d956b898cbc5f074dc6d845518eef0d20
Peter Wagenaar <sakartu@gmail.com>
2015-06-03 11:19:58 +0200

    Add some more SHA1 optimizations

--- util/__init__.py
+++ util/__init__.py
@@ -472,7 +472,7 @@ def is_ascii(b):
         return False
 
 
-def _left_rotate(n, b):
+def left_rotate(n, b):
     return ((n << b) | (n >> (32 - b))) & 0xffffffff
 
 
@@ -499,21 +499,19 @@ def sha1(message):
     # append the bit '1' to the message
     message += b'\x80'
 
-    # append 0 <= k < 512 bits '0', so that the resulting message length (in bits)
-    #    is congruent to 448 (mod 512)
+    # append 0 <= k < 512 bits '0', so that the resulting message length (in bits) is congruent to 448 (mod 512)
     message += b'\x00' * ((56 - (original_byte_len + 1) % 64) % 64)
 
     # append length of message (before pre-processing), in bits, as 64-bit big-endian integer
     message += original_bit_len.to_bytes(8, 'big')
-    # Process the message in successive 512-bit chunks:
-    # break message into 512-bit chunks
+    # Process the message in successive 512-bit (64 byte) chunks:
     for chunk in chunks(message, 64):
-        # break chunk into sixteen 32-bit big-endian words w[i]
-        w = [int.from_bytes(word, 'big') for word in chunks(chunk, 4)] + ([0] * 64)
+        # break chunk into sixteen 32-bit (4 byte) big-endian words
+        w = [int.from_bytes(word, 'big') for word in chunks(chunk, 4)]
 
         # Extend the sixteen 32-bit words into eighty 32-bit words:
-        for j in range(16, 80):
-            w[j] = _left_rotate(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1)
+        for i in range(64):
+            w.append(left_rotate(w[i] ^ w[i + 2] ^ w[i + 8] ^ w[i + 13], 1))
 
         # Initialize hash value for this chunk:
         a = h0
@@ -537,7 +535,7 @@ def sha1(message):
                 f = b ^ c ^ d
                 k = 0xCA62C1D6
 
-            a, b, c, d, e = ((_left_rotate(a, 5) + f + e + k + w[i]) & 0xffffffff, a, _left_rotate(b, 30), c, d)
+            a, b, c, d, e = ((left_rotate(a, 5) + f + e + k + w[i]) & 0xffffffff, a, left_rotate(b, 30), c, d)
 
         # Add this chunk's hash to result so far:
         h0 = (h0 + a) & 0xffffffff
