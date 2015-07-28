df625621d8688fb372e85153b769a4c00585a4f7
Peter Wagenaar <sakartu@gmail.com>
2015-07-28 14:35:39 +0200

    Add wrapper for challenge 32, which starts process with a lower delay

---
+++ ch_32.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+"""
+Usage:
+ch_32.py
+"""
+
+import ch_31
+
+__author__ = 'peter'
+
+
+def main():
+    # Start challenge 31 with a lower delay
+    ch_31.main(delay=0.005)
+
+
+if __name__ == '__main__':
+    main()
